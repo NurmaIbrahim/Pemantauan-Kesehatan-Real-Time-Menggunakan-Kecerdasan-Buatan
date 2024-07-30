501b4769edc57dad67f4c2c02122fcdf3e24013b
NurmaIbrahim <nurmaibrahim2004@gmail.com>
2024-07-30 22:40:09 +0800

    Update alertService.js

--- frontend/src/pages/services/alertService.js
+++ frontend/src/pages/services/alertService.js
@@ -1,1 +1,15 @@
+// alertService.js
+
+import { showAlert, hideAlert } from './alertActions';
+
+// Fungsi untuk menampilkan alert dengan jenis dan pesan
+export const displayAlert = (dispatch, message, alertType, timeout = 5000) => {
+  // Menampilkan alert
+  dispatch(showAlert(message, alertType));
+  
+  // Menyembunyikan alert setelah waktu tertentu
+  setTimeout(() => {
+    dispatch(hideAlert());
+  }, timeout);
+};
 
