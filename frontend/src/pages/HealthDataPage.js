72d8b23e4ad112eca0c062a838fc3cd16b4775a0
NurmaIbrahim <nurmaibrahim2004@gmail.com>
2024-07-30 21:13:52 +0800

    Update HealthDataPage.js

--- frontend/src/pages/HealthDataPage.js
+++ frontend/src/pages/HealthDataPage.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import HealthDataList from './HealthDataList';
 
+const HealthDataPage = () => {
+  const healthData = [
+    {
+      id: 1,
+      name: 'John Doe',
+      age: 30,
+      height: 175,
+      weight: 70,
+      bloodPressure: '120/80',
+      heartRate: 72,
+      bloodSugar: 90,
+    },
