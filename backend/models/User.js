e52c523c00d0b47afb9a3d3d222779ecad32d4b9
NurmaIbrahim <nurmaibrahim2004@gmail.com>
2024-07-30 20:53:43 +0800

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,1 +1,12 @@
+// Contoh implementasi User.js untuk merepresentasikan informasi pengguna
 
+// Definisikan kelas User
+class User {
+  constructor(id, username, email, fullName, birthdate) {
+    this.id = id; // ID unik untuk setiap pengguna
+    this.username = username; // Nama pengguna (username)
+    this.email = email; // Alamat email pengguna
+    this.fullName = fullName; // Nama lengkap pengguna
+    this.birthdate = birthdate; // Tanggal lahir pengguna
+    this.createdAt = new Date(); // Waktu pembuatan akun, diset otomatis saat objek dibuat
+  }
