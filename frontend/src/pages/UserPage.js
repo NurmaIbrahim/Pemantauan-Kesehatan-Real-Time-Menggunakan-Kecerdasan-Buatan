29f6a700f854ebc9e87db863e398030c91422285
NurmaIbrahim <nurmaibrahim2004@gmail.com>
2024-07-30 21:20:34 +0800

    Update UserPage.js

--- frontend/src/pages/UserPage.js
+++ frontend/src/pages/UserPage.js
@@ -1,1 +1,19 @@
+import React from 'react';
+import { useParams, Link } from 'react-router-dom';
 
+const UserPage = ({ users }) => {
+  // Mengambil parameter ID dari URL
+  const { id } = useParams();
+  
+  // Mencari pengguna dengan ID yang sesuai
+  const user = users.find(user => user.id === parseInt(id));
+
+  if (!user) {
+    return (
+      <div className="user-page">
+        <h1>User Not Found</h1>
+        <p>Sorry, the user you are looking for does not exist.</p>
+        <Link to="/users" className="user-page-link">Go to User List</Link>
+      </div>
+    );
+  }
