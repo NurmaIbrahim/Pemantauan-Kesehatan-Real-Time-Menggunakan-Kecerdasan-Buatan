8213005afdfec0e71fc2e017ba34086cf61cdee4
NurmaIbrahim <nurmaibrahim2004@gmail.com>
2024-07-30 21:10:31 +0800

    Update AlertPage.js

--- frontend/src/pages/AlertPage.js
+++ frontend/src/pages/AlertPage.js
@@ -1,1 +1,11 @@
+import React, { useState } from 'react';
+import AlertList from './AlertList';
+
+const AlertPage = () => {
+  const [alerts, setAlerts] = useState([
+    { id: 1, type: 'success', message: 'This is a success alert!' },
+    { id: 2, type: 'danger', message: 'This is a danger alert!' },
+    { id: 3, type: 'warning', message: 'This is a warning alert!' },
+    { id: 4, type: 'info', message: 'This is an info alert!' },
+  ]);
 
