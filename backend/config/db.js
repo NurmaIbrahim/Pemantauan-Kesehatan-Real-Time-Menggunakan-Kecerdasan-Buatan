41254af2a614cf7348949c9f74b61beb76902637
NurmaIbrahim <nurmaibrahim2004@gmail.com>
2024-07-30 20:39:34 +0800

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,15 @@
+const mongoose = require('mongoose');
 
+// Menghubungkan ke database MongoDB menggunakan URL koneksi
+mongoose.connect('mongodb://localhost:27017/nama_database', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+// Mengecek apakah koneksi berhasil
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Koneksi MongoDB gagal:'));
+db.once('open', function () {
+  console.log('Terhubung ke database MongoDB');
+});
