60b1eb2b5fbec0d2a9a6f9ba1f6171f7f9fe4188
NurmaIbrahim <nurmaibrahim2004@gmail.com>
2024-07-30 21:03:23 +0800

    Update AlertList.js

--- frontend/src/assets/images/components/AlertList.js
+++ frontend/src/assets/images/components/AlertList.js
@@ -1,1 +1,12 @@
+import React, { useState, useEffect } from 'react';
 
+const AlertList = ({ alerts }) => {
+  const [visibleAlerts, setVisibleAlerts] = useState(alerts);
+
+  useEffect(() => {
+    setVisibleAlerts(alerts);
+  }, [alerts]);
+
+  const handleDismiss = (id) => {
+    setVisibleAlerts(visibleAlerts.filter(alert => alert.id !== id));
+  }
