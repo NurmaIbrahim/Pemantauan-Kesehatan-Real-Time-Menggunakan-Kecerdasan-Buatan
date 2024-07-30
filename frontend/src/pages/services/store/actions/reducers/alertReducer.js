f14e2e20e8f69c3b31124958ef5ce4e8f9ed37f4
NurmaIbrahim <nurmaibrahim2004@gmail.com>
2024-07-30 22:25:24 +0800

    Update alertReducer.js

--- frontend/src/pages/services/store/actions/reducers/alertReducer.js
+++ frontend/src/pages/services/store/actions/reducers/alertReducer.js
@@ -1,1 +1,12 @@
+// alertReducer.js
+
+// Tindakan (actions) untuk alert
+const SHOW_ALERT = 'SHOW_ALERT';
+const HIDE_ALERT = 'HIDE_ALERT';
+
+// Fungsi tindakan (action creators)
+export const showAlert = (message, alertType) => ({
+  type: SHOW_ALERT,
+  payload: { message, alertType }
+});
 
