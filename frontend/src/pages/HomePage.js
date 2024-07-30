2dbd284ddff0e3139ad91b1aa7438351724e4bfd
NurmaIbrahim <nurmaibrahim2004@gmail.com>
2024-07-30 21:17:06 +0800

    Update HomePage.js

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+const HomePage = () => {
+  return (
+    <div className="home-page">
+      <h1>Welcome to Our Application</h1>
+      <p>This is the main landing page of the application. From here, you can navigate to various sections of the app using the menu.</p>
+      <div className="home-page-links">
+        <Link to="/alerts" className="home-page-link">View Alerts</Link>
+        <Link to="/users" className="home-page-link">View Users</Link>
+        <Link to="/health-data" className="home-page-link">View Health Data</Link>
+      </div>
+    </div>
+  );
+};
