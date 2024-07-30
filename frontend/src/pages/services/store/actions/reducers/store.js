a3dbbbe80a5465b6f9af674a3c66d3ee878bc4b3
NurmaIbrahim <nurmaibrahim2004@gmail.com>
2024-07-30 22:28:06 +0800

    Update store.js

--- frontend/src/pages/services/store/actions/reducers/store.js
+++ frontend/src/pages/services/store/actions/reducers/store.js
@@ -1,1 +1,14 @@
+// store.js
 
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk'; // Middleware untuk async actions
+import logger from 'redux-logger'; // Middleware untuk logging actions (opsional)
+import healthDataReducer from './healthDataReducer'; // Import reducer
+import alertReducer from './alertReducer'; // Import reducer
+
+// Menggabungkan semua reducer
+const rootReducer = combineReducers({
+  healthData: healthDataReducer,
+  alert: alertReducer
+  // Tambahkan reducer lain di sini jika ada
+});
