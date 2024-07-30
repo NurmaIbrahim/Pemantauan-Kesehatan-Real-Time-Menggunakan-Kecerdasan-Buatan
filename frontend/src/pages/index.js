cfadaba6bf8f1423634e16172daae734417669ac
NurmaIbrahim <nurmaibrahim2004@gmail.com>
2024-07-30 21:21:28 +0800

    Update index.js

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './index.css';  // Import CSS global
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
