4e4bf1da03ffb3b3a6124a26d3115877626b7c9c
NurmaIbrahim <nurmaibrahim2004@gmail.com>
2024-07-30 21:06:12 +0800

    Update HealthDataList.js

--- frontend/src/assets/images/components/HealthDataList.js
+++ frontend/src/assets/images/components/HealthDataList.js
@@ -1,1 +1,9 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import HealthDataDetail from './HealthDataDetail';
+
+const HealthDataList = ({ dataList }) => {
+  if (!dataList || dataList.length === 0) {
+    return <div>No health data available.</div>;
+  }
 
