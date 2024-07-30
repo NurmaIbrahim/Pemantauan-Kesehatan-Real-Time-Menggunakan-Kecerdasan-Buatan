d4545229f049b75ccae9ecdb068ba8d7b3bd259a
NurmaIbrahim <nurmaibrahim2004@gmail.com>
2024-07-30 20:41:40 +0800

    Update keys.js

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,1 +1,10 @@
+// Contoh pengaturan koneksi database MongoDB
+module.exports = {
+  mongoURI: 'mongodb://localhost:27017/nama_database', // Ganti dengan URL MongoDB Anda
+};
+
+// Contoh kunci API (jika diperlukan)
+// module.exports = {
+//   apiKey: 'kunci_api_anda_di_sini',
+// };
 
