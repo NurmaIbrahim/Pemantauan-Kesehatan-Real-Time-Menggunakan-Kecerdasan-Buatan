5533641ae398bca7c70b5fbcddbac889bb794249
NurmaIbrahim <nurmaibrahim2004@gmail.com>
2024-07-30 22:37:51 +0800

    Update userActions.js

--- frontend/src/pages/services/store/actions/userActions.js
+++ frontend/src/pages/services/store/actions/userActions.js
@@ -1,1 +1,12 @@
+// userActions.js
 
+// Tindakan (action types)
+export const SET_USER = 'SET_USER';
+export const UPDATE_USER = 'UPDATE_USER';
+export const LOGOUT_USER = 'LOGOUT_USER';
+
+// Fungsi tindakan (action creators)
+export const setUser = (user) => ({
+  type: SET_USER,
+  payload: user
+});
