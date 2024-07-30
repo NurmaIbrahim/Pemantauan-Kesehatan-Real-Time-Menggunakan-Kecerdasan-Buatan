5ecd7b9c031e40b2a60a03b7defc245986a33df6
NurmaIbrahim <nurmaibrahim2004@gmail.com>
2024-07-30 21:08:25 +0800

    Update UserList.js

--- frontend/src/assets/images/components/UserList.js
+++ frontend/src/assets/images/components/UserList.js
@@ -1,1 +1,7 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 
+const UserList = ({ users }) => {
+  if (!users || users.length === 0) {
+    return <div>No users available.</div>;
+  }
