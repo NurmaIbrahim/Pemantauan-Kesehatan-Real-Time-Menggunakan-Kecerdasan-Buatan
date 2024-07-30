541309ce32264814154d4a482483e73ef6a9a310
NurmaIbrahim <nurmaibrahim2004@gmail.com>
2024-07-30 20:52:28 +0800

    Update HealthData.js

--- backend/models/HealthData.js
+++ backend/models/HealthData.js
@@ -1,1 +1,14 @@
+// Contoh implementasi HealthData.js untuk merepresentasikan data kesehatan pengguna
+
+// Definisikan kelas HealthData
+class HealthData {
+  constructor(id, username, date, weight, height, bloodPressure) {
+    this.id = id; // ID unik untuk setiap data kesehatan
+    this.username = username; // Nama pengguna yang terkait dengan data ini
+    this.date = date; // Tanggal pengukuran data kesehatan
+    this.weight = weight; // Berat badan dalam kilogram
+    this.height = height; // Tinggi badan dalam centimeter
+    this.bloodPressure = bloodPressure; // Tekanan darah, misalnya "120/80 mmHg"
+  }
+
 
