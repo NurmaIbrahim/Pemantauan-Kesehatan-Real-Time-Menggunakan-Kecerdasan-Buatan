997ab7f1f988adb36d92f961782a701a2851fb4d
NurmaIbrahim <nurmaibrahim2004@gmail.com>
2024-07-30 22:26:44 +0800

    Update healthDataReducer.js

--- frontend/src/pages/services/store/actions/reducers/healthDataReducer.js
+++ frontend/src/pages/services/store/actions/reducers/healthDataReducer.js
@@ -1,1 +1,12 @@
+// healthDataReducer.js
 
+// Tindakan (actions) untuk health data
+const SET_HEALTH_DATA = 'SET_HEALTH_DATA';
+const UPDATE_HEALTH_DATA = 'UPDATE_HEALTH_DATA';
+const CLEAR_HEALTH_DATA = 'CLEAR_HEALTH_DATA';
+
+// Fungsi tindakan (action creators)
+export const setHealthData = (data) => ({
+  type: SET_HEALTH_DATA,
+  payload: data
+});
