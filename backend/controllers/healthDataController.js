a3167ea345797eee4f90fcb6f4a5f21bdf2f4b70
NurmaIbrahim <nurmaibrahim2004@gmail.com>
2024-07-30 20:47:36 +0800

    Update healthDataController.js

--- backend/controllers/healthDataController.js
+++ backend/controllers/healthDataController.js
@@ -1,1 +1,9 @@
+// Contoh objek data kesehatan
+let healthData = [];
+
+// Fungsi untuk menambahkan data kesehatan baru
+function addHealthData(data) {
+  healthData.push(data);
+  console.log('Data kesehatan berhasil ditambahkan:', data);
+}
 
