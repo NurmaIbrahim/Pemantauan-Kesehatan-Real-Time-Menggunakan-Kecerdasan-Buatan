c3bd85f9812d15549f783efb74e205f19f2dc8f6
NurmaIbrahim <nurmaibrahim2004@gmail.com>
2024-07-30 22:30:07 +0800

    Update alertActions.js

--- frontend/src/pages/services/store/actions/alertActions.js
+++ frontend/src/pages/services/store/actions/alertActions.js
@@ -1,1 +1,12 @@
+// alertActions.js
+
+// Tindakan (action types)
+export const SHOW_ALERT = 'SHOW_ALERT';
+export const HIDE_ALERT = 'HIDE_ALERT';
+
+// Fungsi tindakan (action creators)
+export const showAlert = (message, alertType) => ({
+  type: SHOW_ALERT,
+  payload: { message, alertType }
+});
 
