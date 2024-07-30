45342875db953f89c98afcea4d5bebcb0e6e4c1a
NurmaIbrahim <nurmaibrahim2004@gmail.com>
2024-07-30 20:49:57 +0800

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,1 +1,8 @@
+// Contoh data pengguna
+let users = [];
 
+// Fungsi untuk menambahkan pengguna baru
+function addUser(user) {
+  users.push(user);
+  console.log('Pengguna baru ditambahkan:', user);
+}
