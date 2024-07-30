632ec853361c92f53ec2014e3f75e3f4b51659b7
NurmaIbrahim <nurmaibrahim2004@gmail.com>
2024-07-30 22:36:36 +0800

    Update healthDataActions.js

--- frontend/src/pages/services/store/actions/healthDataActions.js
+++ frontend/src/pages/services/store/actions/healthDataActions.js
@@ -1,1 +1,12 @@
+// healthDataActions.js
 
+// Tindakan (action types)
+export const SET_HEALTH_DATA = 'SET_HEALTH_DATA';
+export const UPDATE_HEALTH_DATA = 'UPDATE_HEALTH_DATA';
+export const CLEAR_HEALTH_DATA = 'CLEAR_HEALTH_DATA';
+
+// Fungsi tindakan (action creators)
+export const setHealthData = (data) => ({
+  type: SET_HEALTH_DATA,
+  payload: data
+});
