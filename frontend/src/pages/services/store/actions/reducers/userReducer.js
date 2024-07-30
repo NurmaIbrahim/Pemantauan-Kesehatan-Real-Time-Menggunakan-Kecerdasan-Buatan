97f0a422455a745ad2fb7b8685ad62ce53fda8cf
NurmaIbrahim <nurmaibrahim2004@gmail.com>
2024-07-30 22:28:55 +0800

    Update userReducer.js

--- frontend/src/pages/services/store/actions/reducers/userReducer.js
+++ frontend/src/pages/services/store/actions/reducers/userReducer.js
@@ -1,1 +1,12 @@
+// userReducer.js
 
+// Tindakan (actions) untuk pengguna
+const SET_USER = 'SET_USER';
+const UPDATE_USER = 'UPDATE_USER';
+const LOGOUT_USER = 'LOGOUT_USER';
+
+// Fungsi tindakan (action creators)
+export const setUser = (user) => ({
+  type: SET_USER,
+  payload: user
+});
