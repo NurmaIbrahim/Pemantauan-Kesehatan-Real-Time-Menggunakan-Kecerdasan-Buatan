4c0ebed55d73bc501b0046bd2909caec17ee7133
NurmaIbrahim <nurmaibrahim2004@gmail.com>
2024-07-30 21:07:24 +0800

    Update Navbar.js

--- frontend/src/assets/images/components/Navbar.js
+++ frontend/src/assets/images/components/Navbar.js
@@ -1,1 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const Navbar = ({ links }) => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <Link to="/">BrandName</Link>
+      </div>
+      <ul className="navbar-links">
+        {links.map((link, index) => (
+          <li key={index} className="navbar-item">
+            <Link to={link.path}>{link.label}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
